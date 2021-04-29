2289d19fc95a75c666b6a87a5c7159843594c75b
rzena <rzena@LAPTOP-EQH8BBJL>
2021-04-29 11:10:40 +0900

    2021깃허브옮기기

---
+++ smart_유제나/src/smart_유제나/Ex02test.java
@@ -0,0 +1,12 @@
+package smart_유제나;
+
+public class Ex02test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Ex01 ex01= new Ex01();
+		ex01.gugu();
+		//java - jar  smart_유제나.jar
+	}
+
+}
